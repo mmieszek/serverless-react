1baee51e5f4bb9225294a470a1fc76caf107c9c5
Marcin Mieszek <marcin.mieszek@gmail.com>
2019-11-22 20:38:51 +0100

    Components structures

---
+++ src/components/Messages/Messages.tsx
@@ -0,0 +1,3 @@
+import React from "react";
+
+export const Messages = () => <div>Messages</div>;
---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export * from "./Header";
+export * from "./Input";
+export * from "./Messages";
+export * from "./Sidebar";
