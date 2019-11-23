9267b21dc18aaf2fa9a44d1abb85283c65a93b58
Marcin Mieszek <marcin.mieszek@gmail.com>
2019-11-23 04:24:40 +0100

    Small fixes

--- src/components/Giphy/Giphy.tsx
+++ src/components/Giphy/Giphy.tsx
@@ -6,14 +6,13 @@ interface Props {
 }
 export const Giphy: FC<Props> = ({ text }) => {
   const [url, setUrl] = useState<string | null>(null);
-  console.log({ text });
   useEffect(() => {
     const fetchData = async () => {
       const data = await API.get("giphyFunction", "/giphy?text=" + text, {});
       setUrl(data.image);
     };
     fetchData();
-  }, []);
+  }, [text]);
   if (!url) return null;
-  return <img src={url} height="100" />;
+  return <img src={url} height="100" alt={text} />;
 };
